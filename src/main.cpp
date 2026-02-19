0c63c7d4df28995e12956d4bea0bcdeec5f4b960
jonathan <jonathhu@stud.ntnu.no>
2026-02-19 20:04:34 +0100

    test: cell class demo

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,22 @@ int main()
 
     std::cout << cell.getValue() << '\n';
 
+    Cell another_cell;
+
+    std::cout << another_cell.getCandidates() << '\n'; // should be 1022
+
+    another_cell.eliminateCandidate(9);
+
+    std::cout << another_cell.getCandidates() << '\n'; // should now be 510
+    another_cell.eliminateCandidate(8);
+    another_cell.eliminateCandidate(7);
+    another_cell.eliminateCandidate(6);
+    another_cell.eliminateCandidate(5);
+    another_cell.eliminateCandidate(4);
+    another_cell.eliminateCandidate(3);
+    another_cell.eliminateCandidate(2);
+
+    std::cout << "Solved = " << another_cell.isSolved() << '\n';
 }
 
 
