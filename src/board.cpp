7f8e642306544e9d968a9eb7070281b9ef7809d3
jonathan <jonathhu@stud.ntnu.no>
2026-02-19 22:09:57 +0100

    feat: creat board class header and constructor

---
+++ src/board.cpp
@@ -0,0 +1,17 @@
+#include "../include/board.h"
+#include <stdexcept>
+
+Board::Board(const std::string& initial_state) {
+    if (initial_state.empty()) {
+        throw std::invalid_argument("Initial_state can't be empty");
+    } 
+
+    for (int i = 0; i < 81; i++) {
+        if (initial_state[i] == '0') {
+            board_[i] = Cell();
+        } else {
+            board_[i] = Cell(initial_state[i] - '0');
+        }
+    }
+    
+}
